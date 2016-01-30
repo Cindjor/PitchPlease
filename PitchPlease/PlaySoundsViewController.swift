7af8595e5aa7aeb569f5b059f7c1b177b9e2cddf
Cindjor <cheung.luke@gmail.com>
2016-01-29 19:32:56 -0800

    variable pitches
    added the proper code for adjusting the pitch of the voice. storyboard
    hasn’t been updated yet

--- PitchPlease/PlaySoundsViewController.swift
+++ PitchPlease/PlaySoundsViewController.swift
@@ -13,6 +13,8 @@ class PlaySoundsViewController: UIViewController {
     
     var audioPlayer = AVAudioPlayer()
     var receivedAudio: RecordedAudio!
+    var audioEngine: AVAudioEngine!
+    var audioFile: AVAudioFile!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +27,9 @@ class PlaySoundsViewController: UIViewController {
 //        }
         audioPlayer = try! AVAudioPlayer(contentsOfURL: receivedAudio.filePathURL)
         audioPlayer.enableRate = true
+        audioEngine = AVAudioEngine()
+        audioFile = try! AVAudioFile(forReading: receivedAudio.filePathURL)
+        
         
     }
     
@@ -41,6 +46,34 @@ class PlaySoundsViewController: UIViewController {
         audioPlayer.stop()
         audioPlayer.rate = 0.62
         audioPlayer.play()
+    }
+    @IBAction func playChipmunkAudio(sender: AnyObject) {
+        playAudioWithVariablePitch(1000)
+    }
+    @IBAction func playVaderAudio(sender: AnyObject) {
+        playAudioWithVariablePitch(-900)
+    }
+    
+    func playAudioWithVariablePitch(pitch: Float){
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        let audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        let changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        try! audioEngine.start()
+        audioPlayerNode.play()
+                
+        
     }
     
 }
